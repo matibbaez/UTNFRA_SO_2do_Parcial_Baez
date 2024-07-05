5d8ed23736674ad77ffcae39ecd68c8ca7212767
Matías Baez <128560900+matibbaez@users.noreply.github.com>
2024-07-05 13:36:36 -0300

    Update baez_AltaUser-Groups.sh

--- RTA_Examen_20240705/PuntoB/baez_AltaUser-Groups.sh
+++ RTA_Examen_20240705/PuntoB/baez_AltaUser-Groups.sh
@@ -1,21 +1,16 @@
-# Verificación de argumentos
 if [ $# -ne 2 ]; then
     echo "Uso: $0 <usuario_clave> <ruta_archivo_usuarios>"
     echo "Ejemplo: $0 usuario1 /path/to/repo/202406/bash_script/Lista_Usuarios.txt"
     exit 1
 fi
 
-# Extraer la clave del usuario del primer parámetro
 usuario_clave="$1"
 clave=$(grep "^$usuario_clave:" "$2" | cut -d: -f2)
 
-# Crear grupo
 grupo="mi_grupo"
 groupadd "$grupo" 2>/dev/null || true
 
-# Crear usuario y asignarle la misma clave
 nuevo_usuario="nuevo_usuario"
 useradd -m -p "$clave" "$nuevo_usuario"
 
-# Agregar usuario al grupo
 usermod -aG "$grupo" "$nuevo_usuario"
